79d5bba2604fa1b9cb575ce2e2cd90d420c37f20
Krishna Sivakumar <krishnasivaprogrammer@gmail.com>
2022-02-27 13:06:06 +0530

    add information overlay to map

--- plot.py
+++ plot.py
@@ -84,15 +84,55 @@ class Map:
 
     def save_map(self):
         self.m.save(self.dist_file)
-    
+
+    def inject_overlay(self, dist_dir, overlay_dir):
+        from bs4 import BeautifulSoup
+        from re import compile
+
+        map_file = BeautifulSoup(
+            open(self.dist_file, "r").read(),
+            "html.parser"
+        )
+        css_file = open(
+            os.path.join(overlay_dir, "overlay.css")
+        ).read()
+
+        overlay = BeautifulSoup(
+            open(
+                os.path.join(overlay_dir, "overlay.html"),
+                "r"
+            ).read(),
+            "html.parser"
+        )
+
+        map_div = map_file.find("div", id=compile("map_"))
+        map_div["style"] = "z-index: 0;"
+        map_div.insert_after(overlay)
+
+        map_file.find("head").append(
+            BeautifulSoup("<link rel='stylesheet' href='overlay.css'>"),
+            "html.parser"
+        )
+
+        with open(self.dist_file, "w", encoding="utf-8") as dist_html:
+            dist_html.write(str(map_file))
+
+        with open(
+                os.path.join(dist_dir, "overlay.css"), "w") as dist_css:
+            dist_css.write(css_file)
+
     def __del__(self):
         del self.m
         del self.geolocator
+
+
 if __name__ == "__main__":
     tweets_file = "twitter.txt"
     dist_dir = "dist"
+    overlay_dir = "overlay-components"
 
     uk = Map(tweets_file, dist_dir)
     uk.generate_map()
     uk.add_borders()
     uk.save_map()
+    uk.inject_overlay(dist_dir, overlay_dir)
