95ae3b657090b2c8523d5d6ac2a267ddaa387b54
Gabriele Calarota <gabriele.calarota@gmail.com>
2022-02-28 12:15:11 +0100

    Refactoring for linting

--- scrape.py
+++ scrape.py
@@ -1,17 +1,20 @@
+"""Scraper class"""
 import json
 
-import tweepy
 import datetime
-from plot import Map
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import tweepy
+from plot import Map
 
 # Load envs from the .env
 load_dotenv()
 
 
 class Scraper:
+    """Scraper class will handle twitter data scraping"""
+
     def __init__(self, bearer_token, temp_dir, dist_dir):
         self.token = bearer_token
         self.client = tweepy.Client(bearer_token)
@@ -20,7 +23,10 @@ class Scraper:
         self.dist_dir = dist_dir
         Path(temp_dir).mkdir(parents=True, exist_ok=True)
         # loading env variable and converting element to int
-        self.blacklist_usernames = [int(el) for el in json.loads(os.getenv('BLACKLIST_USERID', '[]'))]
+        self.blacklist_usernames = [int(el)
+                                    for el in json.loads(os.getenv('BLACKLIST_USERID',
+                                                                   '[]'))
+                                    ]
 
     def _get_user_id(self, username):
         user = self.client.get_user(username=username)
@@ -43,32 +49,39 @@ class Scraper:
                 continue
             if verbose:
                 print(tweet_data)
-            with open(self.tweets_file, "a") as f:
-                f.write(str(tweet_data))
-                f.write("\n")
+            with open(self.tweets_file, "a", encoding="utf-8") as output_file:
+                output_file.write(str(tweet_data))
+                output_file.write("\n")
 
-    def update_query(self, hashtags, keywords, preposition, add_args="-is:retweet"):
+    def update_query(self, hashtags_list, keywords, preposition, add_args="-is:retweet"):
+        """Updating query"""
         print("Query updated!")
-        str1 = f"({' OR '.join(hashtags)})"
+        str1 = f"({' OR '.join(hashtags_list)})"
         str2 = f"({' OR '.join(keywords)})"
         str3 = f"({' OR '.join(preposition)})"
         str4 = add_args
         self.query = " ".join([str1, str2, str3, str4])
 
     def scrap_query(self, time_limit=10, verbose=False, **kwargs):
+        """Scraping query from recent tweets"""
         if not self.query:
             print("Please update query first!")
             return
         now = datetime.datetime.now(datetime.timezone.utc)
         start = now - datetime.timedelta(minutes=time_limit)
         start = start.isoformat(timespec="seconds")
-        kwargs.update({"user.fields": "created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,"
+        kwargs.update({"user.fields": "created_at,description,entities,id,"
+                                      "location,name,pinned_tweet_id,profile_image_url,"
                                       "protected,public_metrics,url,username,verified,withheld"})
-        tweets = self.client.search_recent_tweets(self.query, start_time=start, expansions='author_id', **kwargs)
+        tweets = self.client.search_recent_tweets(self.query,
+                                                  start_time=start,
+                                                  expansions='author_id',
+                                                  **kwargs)
         self._write_tweets(tweets, verbose)
         print("Done scraping query.")
 
     def scrape_users(self, usernames, time_limit=10, verbose=False, **kwargs):
+        """Scraping users"""
         now = datetime.datetime.now(datetime.timezone.utc)
         start = now - datetime.timedelta(minutes=time_limit)
         start = start.isoformat(timespec="seconds")
@@ -86,23 +99,24 @@ class Scraper:
             print("Done scraping users.")
 
     def plot_map(self):
+        """Plotting map"""
         print("Creating Map...")
-        uk = Map(self.tweets_file, self.dist_dir)
-        uk.generate_map()
-        uk.add_borders()
-        uk.save_map()
-        uk.inject_overlay(self.dist_dir, "overlay-components")
+        ukraine_map = Map(self.tweets_file, self.dist_dir)
+        ukraine_map.generate_map()
+        ukraine_map.add_borders()
+        ukraine_map.save_map()
+        ukraine_map.inject_overlay(self.dist_dir, "overlay-components")
         print("Plotted!")
-        del uk
+        del ukraine_map
 
 
 # EXAMPLE
 if __name__ == "__main__":
-    bearer_token = os.environ["BEARER"]
-    s = Scraper(bearer_token, "temp", "dist")
-    hashtags = ["#ukraine", "#russianarmy","#OSINT"]
+    s = Scraper(os.environ["BEARER"], "temp", "dist")
+    hashtags = ["#ukraine", "#russianarmy", "#OSINT"]
     prepositions = ['near', '"south of"', '"north of"', '"east of"', '"west of"']
-    key_words = ['spotted', 'movement', 'soldiers', 'attacks', 'army', 'military', 'vehicles', 'aircraft', 'plane',
+    key_words = ['spotted', 'movement', 'soldiers', 'attacks',
+                 'army', 'military', 'vehicles', 'aircraft', 'plane',
                  'shoot', 'shell', 'fight', 'invaders', 'strike', 'tank']
     s.update_query(hashtags, key_words, prepositions)
     s.scrap_query(time_limit=100)
